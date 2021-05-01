0cda3ae155b9636d07447e51abcc0c89cc6b7a3f
Mambaur <alwiroziq1998@gmail.com>
2021-05-02 00:16:46 +0700

    update configurasi dompdf

--- config/app.php
+++ config/app.php
@@ -228,7 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'QRCode' => LaravelQRCode\Facades\QRCode::class,
-
+        'PDF' => Barryvdh\DomPDF\Facade::class
     ],
 
 ];
